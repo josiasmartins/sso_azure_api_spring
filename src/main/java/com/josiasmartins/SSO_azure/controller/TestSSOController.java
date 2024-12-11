39441e53bc3e3299344586620b4a2ff11c6a14b4
josias.m.caitano <josias.m.caitano@accenture.com>
2024-12-11 09:26:03 -0300

    fix: fixed SSO in project

--- src/main/java/com/josiasmartins/SSO_azure/controller/TestSSOController.java
+++ src/main/java/com/josiasmartins/SSO_azure/controller/TestSSOController.java
@@ -2,39 +2,21 @@ package com.josiasmartins.SSO_azure.controller;
 
 import com.josiasmartins.SSO_azure.domain.model.DevoncaModel;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class TestSSOController {
 
-//    @GetMapping
-//    @ResponseStatus(HttpStatus.OK)
-//    public DevoncaModel test() {
-//
-//        DevoncaModel devoncaModel = new DevoncaModel();
-//        devoncaModel.setName("test");
-//        devoncaModel.setCurrent(false);
-//
-//        return devoncaModel;
-//
-//    }
-
-    public String index(Model model, Authentication user) {
-        model.addAttribute("user", user);
-
-        return "http://localhost/all";
-    }
-
-    @GetMapping("/all")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String test() {
-
-        return "http://localhost/all";
+    public DevoncaModel test() {
+        DevoncaModel devoncaModel = new DevoncaModel();
+        devoncaModel.setName("test");
+        devoncaModel.setCurrent(false);
+        return devoncaModel; // Retorna o objeto como JSON
     }
 }
